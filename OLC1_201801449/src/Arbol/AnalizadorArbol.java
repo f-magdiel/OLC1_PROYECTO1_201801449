155892aa14cade8e92cebd25a1036e209771fd45
magdielasicona <32757541405@ingenieria.usac.edu.gt>
2022-03-02 00:22:56 -0600

    arbol semiterminado

--- OLC1_201801449/src/Arbol/AnalizadorArbol.java
+++ OLC1_201801449/src/Arbol/AnalizadorArbol.java
@@ -1,18 +1,98 @@
 package Arbol;
-
+import Arbol.Nodo;
 /**
  *
  * @author magdiel
  */
-public class AnalizadorArbol {
+public class AnalizadorArbol{
+    public Nodo raiz;
+    public Nodo aux1;
+    public Nodo aux2;
     
+    public int contadorid=0;
+    public AnalizadorArbol(){
+        this.raiz = null;
+        this.aux1=null;
+        this.aux2=null;
+    }
+    
+    public void insercion(String cadenaser){
+        this.contadorid++;
+        this.raiz = insertarArbol(this.raiz,cadenaser,this.contadorid);
+        
+    }
+    
+    
+    public Nodo insertarArbol(Nodo nodo,String caracter,int id){
+        
+        if(nodo==null){ // para ver si raiz es null
+            nodo = new Nodo(null,null," ",0);
+            nodo.valor= " ";
+            nodo.nododer = new Nodo(null,null,caracter,id);
+            
+        }else{// si raiz no es null
+            if(caracter.equals(".") || caracter.equals("|")){
+                nodo.valor = caracter;//el nodo principal es seteado
+            }else if(caracter.equals("+") || caracter.equals("*") || caracter.equals("?")){
+                if(nodo==null){//si todavia hay espacio
+                    nodo.valor = caracter;
+                    //se crea otra rama
+                    aux1 = nodo;
+                    Nodo nuevonodo = new Nodo(null,null," ",0);
+                    nuevonodo.nododer = aux1;
+                    nodo = nuevonodo;
+                    aux1=null;
+                }else{//ya no hay espacio es necesario crear otra rama
+                    aux1 = nodo;
+                    Nodo nuevonodo = new Nodo(null,null,caracter,id);//cuando ya no hay espacio y se setea *,?,+
+                    nuevonodo.nododer = aux1;
+                    nodo = nuevonodo;
+                    aux1=null; //retorna el nodo
+                    
+                    // se crea nueva rama otra vez
+                    aux1 = nodo;
+                    Nodo otronodo = new Nodo(null,null," ",0);
+                    otronodo.nododer = aux1;
+                    nodo = otronodo;
+                    aux1= null;
+                }
+                
+            }else{
+                if(nodo.nododer==null){
+                    nodo.nododer = new Nodo(null,null,caracter,0);
+                }else if(nodo.nodoizq==null){
+                    nodo.nodoizq = new Nodo(null,null,caracter,0);
+                }else{//se crea una rama nuevo
+                    aux1 = nodo;
+                    Nodo nuevonodo = new Nodo(null,null," ",id);
+                    nuevonodo.nododer = aux1;
+                    nuevonodo.nodoizq = new Nodo(null,null,caracter,0);
+                    nodo = nuevonodo;
+                    aux1=null;
+                    
+                }
+            }
+            
+        }
+        return nodo;
+    }
+    //************************************ANALIZADOR ER***********************
     public void entradaAnalizador(String expresionregular){
         String[] alfabeto = this.analizadorArbol(expresionregular);
+       
         System.out.println("SEPERADOS**************************");
-        for (int i = 0; i < alfabeto.length; i++) {
-            System.out.println(alfabeto[i]);
+        for (int i = alfabeto.length-1; i >= 0; i--) {
+            //System.out.println(alfabeto[i]);
+            this.insercion(alfabeto[i].replace(" ",""));
         }
+        //para cerrar arbol
+        Nodo nuevonodo = new Nodo(null,null,".",0);
+        nuevonodo.nodoizq = this.raiz;
+        nuevonodo.nododer = new Nodo(null,null,"#",0);
+        this.raiz = nuevonodo;
+        this.raiz = null;
         System.out.println("FIN*********************************");
+        //se agrega el . y el # al final del arbol  
     }
     
     public String[] analizadorArbol(String er){
@@ -74,3 +154,6 @@ public class AnalizadorArbol {
         }
     }
 }
+
+
+
--- OLC1_201801449/src/Arbol/Nodo.java
+++ OLC1_201801449/src/Arbol/Nodo.java
@@ -9,7 +9,7 @@ package Arbol;
  *
  * @author magdiel
  */
-public class Nodo {
+public class Nodo{
     public String valor;
     public int id;
     public Nodo nodoizq;
@@ -19,8 +19,8 @@ public class Nodo {
     public String anulable;
     
     public Nodo(Nodo _nodoizq,Nodo _nododer,String _valor,int _id){
-        this.nododer = _nododer;
-        this.nodoizq = _nodoizq;
+        this.nododer = null;
+        this.nodoizq = null;
         this.id = _id;
         this.valor = _valor;
         this.anulable = "";
@@ -28,4 +28,77 @@ public class Nodo {
         this.last = "";
     }
     
+    public String getValor(){
+        return this.valor;
+    }
+    
+    public int getId(){
+        return this.id;
+    }
+    
+    public Nodo getNodoIzquierdo(){
+        return this.nodoizq;
+    }
+    
+    public Nodo getNodoDerecho(){
+        return this.nododer;
+    }
+    
+    public String getFirst(){
+        return this.first;
+    }
+    
+    public String getLast(){
+        return this.last;
+    }
+    
+    public String getAnulable(){
+        return this.anulable;
+    }
+    
+    public void setValor(String _valor){
+        this.valor = _valor;
+    }
+    
+    public void setId(int _id){
+        this.id = _id;
+    }
+    
+    public void setNodoIzquierdo(Nodo _nodoizq){
+        this.nodoizq = _nodoizq;
+    }
+    
+    public void setNodoDerecho(Nodo _nododer){
+        this.nododer = _nododer;
+    }
+    
+    public void setFirst(String _first){
+        this.first = _first;
+    }
+    
+    public void setLast(String _last){
+        this.last = _last;
+    }
+    
+    public void setAnulable(String _anulable){
+        this.anulable = _anulable;
+    }
+    
+    public String getNodosEstructura(){
+           String etiqueta;
+        if (nodoizq == null && nododer == null) {
+            etiqueta = "nodo" + id + " [ label =\"" + valor+"\"];\n";
+        } else {
+            etiqueta = "nodo" + id + " [ label =\"" + valor + "\"];\n";
+        }
+        if (nodoizq != null) {
+            etiqueta = etiqueta + nodoizq.getNodosEstructura()
+                    + "nodo" + id + "->nodo" + nodoizq.id + "\n";
+        }
+        if (nododer != null) {
+            etiqueta = etiqueta + nododer.getNodosEstructura()
+                    + "nodo" + id + "->nodo" + nododer.id + "\n";
+        }
+        return etiqueta;
+    }
 }
